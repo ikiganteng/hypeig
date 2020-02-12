a6a7fa893343ab89c1a6b8fd32eb8d3ba0f2f337
Andi Muh. Rizqi <ikiganteng@gegsulbar.com>
2020-02-13 05:10:32 +0700

    Create likehome.php

---
+++ likehome.php
@@ -0,0 +1,98 @@
+<?php
+error_reporting(0);
+set_time_limit(0);
+date_default_timezone_set('UTC');
+
+require __DIR__ . '/vendor/autoload.php';
+
+/////// CONFIG ///////
+$username = 'usernamelu!';
+$password = 'passwordlu!';
+$debug = false; // untuk mengecek ubah ke true
+$truncatedDebug = false;
+$mycount = 0;
+$homecount = 0;
+//////////////////////
+
+$climate = new \League\CLImate\CLImate();
+$ig = new \InstagramAPI\Instagram($debug, $truncatedDebug);
+$climate->green()->bold(
+    "
+    _ __   _                   __                  
+   (_) /__(_)___ _____ _____  / /____  ____  ____ _
+  / / //_/ / __ `/ __ `/ __ \/ __/ _ \/ __ \/ __ `/
+ / / ,< / / /_/ / /_/ / / / / /_/  __/ / / / /_/ / 
+/_/_/|_/_/\__, /\__,_/_/ /_/\__/\___/_/ /_/\__, /  
+         /____/                           /____/  
+"
+);
+$climate->out('');
+$climate->green()->bold('BOT Instagram');
+$climate->green()->bold('Like Home');
+$climate->green()->bold('v1.2'); // menambah kecepatan & menambah delay bypass limits
+$climate->out('');
+$climate->green('© Developed by IKIGANTENG (https://github.com/ikiganteng)');
+$climate->out('');
+try {
+    $ig->login($username, $password);
+   }catch (\Exception $e){
+    echo 'Something went wrong: '.$e->getMessage()."\n";
+    exit(0);
+}
+    $climate->infoBold('Logged as @' . $username . ' successfully.');
+	  $climate->out('');
+
+while(true){
+    try {
+    $feed = $ig->timeline->getTimelineFeed();
+    $itemsJson = json_decode($feed);
+	  $ikiganteng = rand(5, 9);
+    $counsa = 100 % $ikiganteng;
+	  $vprocess = $climate->progress()->total(($counsa * $ikiganteng));
+	  do {
+    $vprocess->advance(floor($counsa), 'Collecting feed from home @'.$username);
+    sleep(1);
+    $ikiganteng -= 1;
+    } while (0 != $ikiganteng);
+    $climate->info('feed home collected.');
+	  $climate->out('');
+    $homecount++;
+    $items = $itemsJson->feed_items;
+    foreach ($items as $item) {
+    $mediaFeed = $item->media_or_ad;
+    $media_id = $mediaFeed->id;    
+    //file_put_contents("feed.txt", json_encode(array('id' => $mediaid)));
+    //$cek = file_get_contents("feed.txt");
+    //$cek = json_decode($cek);
+    //$media_id = $cek->id;
+    $resp = $ig->media->like($media_id,1);
+    $response = json_decode($resp);
+    if ($response->status == 'ok') {
+    $mycount++;
+    $climate->cyan(date('H:i:s') .  ' - Media_id : ' . $media_id . ' Likes Given: ' . $mycount . ' Total Get Home : ' . $homecount);
+    }else{
+    $climate->error(date('H:i:s') .' - Fail to like ');
+    }
+    $counter3 = rand(40, 60) + rand(1, 9); // jgn ubah ajg
+    $climate->darkGray('Starting ' . $counter3 . ' second(s) delay for bypassing Instagram limits.');
+    $vProgress = $climate->progress()->total($counter3);
+    do{
+    $vProgress->advance(1, $counter3. 'second(s) left');
+    sleep(1);
+    $counter3 -= 1;
+    } while (0 != $counter3);
+    }
+	  $climate->out('');
+    $counter3 = 1800; // jgn ubah ajg
+    $climate->error('Starting ' . $counter3 . ' second(s) delay for sleep.');
+    $vProgress = $climate->progress()->total($counter3);
+    do {
+    $vProgress->advance(1, $counter3. 'second(s) left');
+    sleep(1);
+    $counter3 -= 1;
+    } while (0 != $counter3);
+    $climate->out('');
+    } catch (\Exception $e) {
+    echo 'Something went wrong: '.$e->getMessage()."\n";
+    }
+}
